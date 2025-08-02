fd7aa00d71fd70fe71eef256e0a6dc3d958e9ef3
tanviip1809-code <tanviip1809@gmail.com>
2025-08-02 15:09:18 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,27 @@
+"Use Strict"; //treat all JS code as a newer version
+
+//alert(3 + 3) //we are using node js, not browser so arithmetic op is not allowed]
+
+console.log(3 + 
+    3); //code readability should be high
+
+
+let name = "Tanu";
+let age = 21;
+let isLoggedIn = false;
+let state;
+
+//number => range - 2 to power 53 (approx)
+//bigInt 
+//string => ""
+//boolean => true or false
+//null => standalone value //it is empty //null is a object
+//undefined =>  
+//symbol => unique 
+
+//object
+
+console.log(typeof 18); //op : number
+console.log(typeof undefined); //op : undefined
+console.log(typeof null); //op : object
+
