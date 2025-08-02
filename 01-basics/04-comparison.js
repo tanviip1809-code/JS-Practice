245a2215f5413ce4bb09ac8e0a5a68e75bbb45f6
tanviip1809-code <tanviip1809@gmail.com>
2025-08-02 18:15:49 +0000

    comparisons

---
+++ 01-basics/04-comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1); //both should be in same datatype
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// === 
+console.log("2" === 2); //it is strict - checks datatypes too, both datatypes should be same for ===
+
+
+
+
+
