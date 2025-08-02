6e716d5d5cd846511a99cf5363090f16a5ce3339
tanviip1809-code <tanviip1809@gmail.com>
2025-08-02 17:33:57 +0000

    conversions of datatypes

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -16,7 +16,7 @@ let valueInNumber = Number(score) //here string is converted into number
 
 let isLoggedIn = "tanu"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //"" => false
@@ -24,5 +24,40 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ********************* Operations **************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "tanu"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //op: 12
+// console.log(1 + "2"); //op: 12
+// console.log("1" + 2 + 2); //op: 122 => first string so afterwards coverted into string
+// console.log(1 + 2 + "3"); //op: 33 => first it has number so it will add and the converted into string
+// console.log((3 + 4) * 5 % 3); // this is bad syntax
+
+console.log(+true); //1 => because of true
+console.log(+""); //0 => because it has empty string
+
+let num1, num2, num3
+num1 = num2 = num3 =  2+2+2
+console.log(num1, num2, num3); //it is not recommend 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //101
