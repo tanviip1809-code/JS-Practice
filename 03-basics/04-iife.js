2be1a732c03f4b9eac9531ac553381088748bf36
tanviip1809-code <tanviip1809@gmail.com>
2025-08-13 06:36:19 +0000

    IIFE

--- 03-basics/04-iife.js
+++ 03-basics/04-iife.js
@@ -3,7 +3,6 @@
 //WHY IIFE? -> there is a many problem happen by global scope pollution, so to remove that pollution which is happen by global scope variables or declaration or by any other thing, we use iife 
 
 //Create a private scope
-
 //In IIFE, it is necessary to end that function by using semicolon
 
 (function chai() {
