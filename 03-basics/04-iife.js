8e1cc5fe296ee1083008b0f1de1c26fbd29a9190
tanviip1809-code <tanviip1809@gmail.com>
2025-08-13 05:51:30 +0000

    IIFE

---
+++ 03-basics/04-iife.js
@@ -0,0 +1,19 @@
+//Immediately Invoked Function Expression (IIFE)
+
+//WHY IIFE? -> there is a many problem happen by global scope pollution, so to remove that pollution which is happen by global scope variables or declaration or by any other thing, we use iife 
+
+//Create a private scope
+
+//In IIFE, it is necessary to end that function by using semicolon
+
+(function chai() {
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+//syntax
+((name) => {
+    //unnamed IIFE
+    console.log(`DB CONNECTED TWO ${name}`)
+})('tanu')
+
