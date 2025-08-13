d9cc344c9a095d623ab3e7284ed9ca974b8b956e
tanviip1809-code <tanviip1809@gmail.com>
2025-08-13 07:00:42 +0000

    conditional

---
+++ 04-control_flow/one.js
@@ -0,0 +1,18 @@
+//If statement - condition should be true, then and then internal code wil be run, if false then we will not enter in the code
+
+//syntax
+// if (condition) {
+// }
+
+const isUserLoggedIn = true
+if (isUserLoggedIn) {
+
+}
+
+const temperature = 41
+if (temperature === 41) {
+    console.log("less than 50");
+}
+console.log("temperature is greater than 50");
+
+// <, >, <=, >=, ==, !=, ===, !==
