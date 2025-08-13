21c3a3bc2c02e3ef070d2f3a0537181c531d4693
tanviip1809-code <tanviip1809@gmail.com>
2025-08-13 16:46:07 +0000

    Switch Statement

---
+++ 04-control_flow/switch.js
@@ -0,0 +1,34 @@
+//Switch Case 
+
+//Syntax
+// switch (key) {
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("June")
+        break;
+    default:
+        console.log("default case match")
+        break;
+}
+
