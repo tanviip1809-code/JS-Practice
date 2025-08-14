f14964da0c0352888243b9a603473530b570f67c
tanviip1809-code <tanviip1809@gmail.com>
2025-08-14 15:28:44 +0000

    While & Do...While

---
+++ 05-iterations/two.js
@@ -0,0 +1,34 @@
+// While Loop
+
+//Syntax
+// while (condition) { 
+// }
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index ix: ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ['flash', 'batman', 'superman']
+let i = 0
+while (i < myArray.length) {
+    console.log(myArray[i]);
+    i++
+}
+
+
+//Do...While - atleast runs once
+
+//Syntax
+// do {
+// } while (condition);
+
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
