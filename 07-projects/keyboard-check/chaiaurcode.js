0715e63c282295949fafa0efa30323ef542a995f
tanviip1809-code <tanviip1809@gmail.com>
2025-10-28 13:41:47 +0000

    key tracker

---
+++ 07-projects/keyboard-check/chaiaurcode.js
@@ -0,0 +1,25 @@
+const insert = document.getElementById('insert');
+
+insert.innerHTML = `
+     <div class='color'>
+        <table id='keyData'>
+           <tr>
+             <th>Key</th>
+             <th>KeyCode</th>
+             <th>Code</th>
+            </tr>
+        </table>
+        </div>
+    `;
+
+const keyTable = document.getElementById('keyData');
+
+window.addEventListener('keydown', function (e) {
+    const row = document.createElement('tr')
+    row.innerHTML = `
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+    `;
+    keyData.appendChild(row);
+})
